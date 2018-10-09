6bc1dae3c8f832bf3277c6aae5863cd3dba0a59a
Vladimir Dzyubin <vdzyubin@spilna.sprava.net>
2018-10-09 12:04:16 +0300

    added check for already visited URLs

--- functions.js
+++ functions.js
@@ -1,15 +1,18 @@
 const https = require('https')
 const fs = require('fs')
-const util = require('util')
 const url = require('url')
-
 const { table } = require('table')
 const getHrefs = require('get-hrefs')
 
+/**
+ * Gets page content as a string for specified siteUrl
+ * @param siteUrl string
+ * @returns {Promise}
+ */
 module.exports.getPageContent = (siteUrl) => {
   return new Promise((resolve, reject) => {
-    const { protocol, host, path } = siteUrl
-    const options = { protocol, host, path }
+    const { protocol, host, path, query } = siteUrl
+    const options = { protocol, host, path, query }
 
     if (protocol === 'https:') options.port = 443
 
@@ -34,28 +37,18 @@ module.exports.getPageContent = (siteUrl) => {
   })
 }
 
-module.exports.writeNumberOfHits = (siteContent, siteUrl, searchPhrase) => {
+/**
+ * Writes search results into result.txt and logs data in tabulated form to stdout
+ * @param pageContent string with HTML of the page
+ * @param siteUrl
+ * @param searchPhrase
+ */
+module.exports.writeNumberOfHits = (pageContent, siteUrl, searchPhrase) => {
   let re = new RegExp(searchPhrase, "gi");
   let result, indices = [];
-  while ( (result = re.exec(siteContent)) ) {
+  while ( (result = re.exec(pageContent)) ) {
     indices.push(result.index)
   }
-
-  /*const data = [
-    [siteUrl.host + siteUrl.path, searchPhrase, `${indices.length} hits!`]
-  ]
-
-  const config = {
-    columns: {
-      0: {
-        width: 50,
-        truncate: 50
-      }
-    }
-  }
-
-  const output = table(data, config)
-  console.log(output)*/
 
   logTabulatedResult(siteUrl, searchPhrase, indices.length)
 
@@ -69,12 +62,25 @@ module.exports.writeNumberOfHits = (siteContent, siteUrl, searchPhrase) => {
   )
 }
 
-module.exports.getUrl = (pageContent, siteUrl, searchPhrase) => {
+/**
+ * Parses page content and returns URL of the next page to search searchPhrase at
+ * @param pageContent string with HTML of the page
+ * @param siteUrl
+ * @param searchPhrase
+ * @param visitedURLs array
+ * @returns object | null URL
+ */
+module.exports.getUrl = (pageContent, siteUrl, searchPhrase, visitedURLs) => {
   const allHrefs = getHrefs(pageContent, { forceHttps: true })
   const re = new RegExp(searchPhrase)
 
   let hrefs = allHrefs.filter(href => {
-    return (re.exec(href) && href.indexOf('.jpg') < 0)
+    return (
+      re.exec(href)
+      && href.indexOf('.jpg') < 0
+      && href.indexOf('#') !== 0
+      && visitedURLs.indexOf(href) === -1
+    )
   })
 
   hrefs = hrefs.filter(href => {
@@ -83,9 +89,19 @@ module.exports.getUrl = (pageContent, siteUrl, searchPhrase) => {
     return siteUrl.host !== hrefUrlObj.host
   })
 
-  return url.parse(hrefs[0])
+  for (let i = 0; i < hrefs.length; i++) {
+    if (hrefs[i]) return { urlObject: url.parse(hrefs[i]), href: hrefs[i] }
+  }
+
+  return null
 }
 
+/**
+ * Logs URL, searchPhrase and number of hits (found searchPhrase on the page) to stdout
+ * @param siteUrl
+ * @param searchPhrase
+ * @param indicesCount Number of hits
+ */
 function logTabulatedResult(siteUrl, searchPhrase, indicesCount) {
   const data = [
     [siteUrl.host + siteUrl.path, searchPhrase, `${indicesCount} hits!`]
--- node.js
+++ node.js
@@ -1,44 +1,21 @@
-const express = require('express')
 const url = require('url')
 const fs = require('fs')
-const https = require('https')
-const util = require('util')
 
 const searchUtils = require('./functions')
 
 let siteUrl = ""
 let searchDepth = 3
 let searchPhrase = ""
+let initialSearchDepth = 0
+let visitedURLs = []
 
 process.argv.forEach(function (val, index, array) {
-  // console.log(index + ': ' + val);
-
   siteUrl = url.parse(array[2])
   searchPhrase = array[3]
   searchDepth = parseInt(array[4], 10)
+  initialSearchDepth = parseInt(array[4], 10)
 });
 
-/*searchUtils.getPageContent(siteUrl)
-  .then(res => {
-    if (searchDepth === 0) return
-
-    // console.log(res)
-    // console.log(siteUrl)
-    pageContent = res
-
-    searchUtils.writeNumberOfHits(pageContent, siteUrl, searchPhrase)
-
-    let nextUrl = searchUtils.getUrl(pageContent, searchPhrase)
-    // console.log(nextUrl)
-    if (!nextUrl.host) nextUrl.host = siteUrl.host
-    // console.log(nextUrl)
-
-    siteUrl = searchUtils.getUrl
-
-
-    // siteUrl = nextUrl
-    // console.log(siteUrl)
-  })*/
 try {
   fs.appendFile('result.txt', `===== Search results for <<${searchPhrase}>> =====\n`, () => {})
   search(siteUrl, searchDepth)
@@ -47,24 +24,41 @@ try {
   throw e
 }
 
+/**
+ * Searches number of times searchPhrase is encountered on the page and pages initial page has links to. Writes data
+ * about the search results into result.txt
+ * @param siteUrl string URL to search phrase on
+ * @param searchDepth number Number of links to go through to search searchPhrase
+ */
 function search(siteUrl, searchDepth) {
   if (searchDepth < 1) {
-    console.log('Saved to result.txt')
+    console.log('Search results saved to result.txt')
     fs.appendFile('result.txt', `===== End of search =====\n\n\n`, () => {})
     return
   }
 
   searchUtils.getPageContent(siteUrl)
     .then(pageContentRes => {
-
       searchUtils.writeNumberOfHits(pageContentRes, siteUrl, searchPhrase)
 
-      let nextUrl = searchUtils.getUrl(pageContentRes, siteUrl, searchPhrase)
-      if (!nextUrl.host) nextUrl.host = siteUrl.host
+      const nextUrlData = searchUtils.getUrl(pageContentRes, siteUrl, searchPhrase, visitedURLs)
+
+      if (nextUrlData) {
+        visitedURLs.push(nextUrlData.href)
+      }
+
+      if (!nextUrlData || !nextUrlData.urlObject) {
+        console.log(`Stopped at level of depth ${initialSearchDepth - searchDepth + 1}. No more links with '${searchPhrase}'\n`)
+        console.log('Search results saved to result.txt')
+        fs.appendFile('result.txt', `===== End of search =====\n\n\n`, () => {})
+        return
+      }
+
+      if (!nextUrlData.urlObject.host) nextUrlData.urlObject.host = siteUrl.host
 
       searchDepth--
 
-      search(nextUrl, searchDepth)
+      search(nextUrlData.urlObject, searchDepth)
     })
     .catch(err => {
       console.log(err)
